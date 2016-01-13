0c2541306c816eaf00de3ef27c9673f87d253d17
adam <aborowski@atlassian.com>
2016-01-13 14:24:59 +0100

    best test parameters to overall optimization

--- tester/test.py
+++ tester/test.py
@@ -1,8 +1,13 @@
 import modules.runner as runner
 
 base_config = {
-    'n': 1,
-    'e': 4000,
+    'n': 6,
+    'e': 10000,
+    'c': 100000,
+    'p': 100,
+    'f': 3,
+    'q': 3,
+    'i': 1000
 }
 
 result = runner.test_repeat(base_config, runner.merge_dicts({'o': None}, base_config), 10)
