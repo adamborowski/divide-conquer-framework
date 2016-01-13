ca58b90921969405e3c7213f013a00ca41531400
adam <aborowski@atlassian.com>
2016-01-13 03:01:48 +0100

    test parameter change

--- tester/test.py
+++ tester/test.py
@@ -5,7 +5,7 @@ base_config = {
     'e': 10000,
     'c': 10000,
     'i': 1000,
-    'p': 100
+    'p': 1000
 }
 
 result = runner.test_repeat(base_config, runner.merge_dicts({'o': None}, base_config), 10)
